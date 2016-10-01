53e7046f3e4bb41763193b9888837dffdc34d7e5
RossRich <leschenkobogdan@gmail.com>
2016-10-01 16:00:29 +0300

    create page concrete_product

--- js/concrete_product.js
+++ js/concrete_product.js
@@ -1,33 +1,6 @@
 jQuery(function($) {
 
-    $('.dev-order-input').focus(function() {
-        $(this).parent().addClass('is-focused has-label');
-    })
-
-    $('.dev-order-input').blur(function() {
-        $parent = $(this).parent();
-        if ($(this).val() == "") {
-            $parent.removeClass('is-focused has-label');
-        }
-        $parent.removeClass('is-focused');
-    })
-
-
-
-    $('.dev-footer-input').focus(function() {
-        $(this).parent().addClass('is-focused has-label');
-    })
-
-    $('.dev-footer-input').blur(function() {
-        $parent = $(this).parent();
-        if ($(this).val() == "") {
-            $parent.removeClass('is-focused has-label');
-        }
-        $parent.removeClass('is-focused');
-    });
-
     var ent = ["cement", "crushedStone", "sand", "water"];
-
     ent.forEach(function(item, i) {
         var doughnutData = [];
         var container = "";
@@ -67,13 +40,13 @@ jQuery(function($) {
                 value: 80,
                 color: "#bdbdbd"
             }, ];
-
         }
         var countries = document.getElementById(container).getContext("2d");
         var doughnutOptions = {
             segmentShowStroke: true,
             animateScale: true,
         };
+
         new Chart(countries).Doughnut(doughnutData, doughnutOptions);
     });
 });
