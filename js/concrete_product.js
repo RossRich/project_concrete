b75982f9a6a8da01fa5c7c323aabf5fd33b28b0a
RossRich <leschenkobogdan@gmail.com>
2016-10-02 22:53:12 +0300

    completed page concrete_product and correct footer

--- js/concrete_product.js
+++ js/concrete_product.js
@@ -1,52 +1,22 @@
 jQuery(function($) {
+    $(function() {
+        $('.chart').easyPieChart({
+            barColor: '#757575',
+            trackColor: '#bdbdbd',
+            scaleColor: '#FFF',
+            scaleLength: 0,
+            lineCap: 'round',
+            lineWidth: 12,
+            trackWidth: undefined,
+            size: 110,
+            rotate: 0,
+            easing: 'easeOutBounce',
+            animate: {
+                duration: 1000,
+                enabled: true
+            },
+        });
 
-    var ent = ["cement", "crushedStone", "sand", "water"];
-    ent.forEach(function(item, i) {
-        var doughnutData = [];
-        var container = "";
-        if (item == "cement") {
-            container = item;
-            doughnutData = [{
-                value: 20,
-                color: "#757575"
-            }, {
-                value: 80,
-                color: "#bdbdbd"
-            }, ];
-        } else if (item == "crushedStone") {
-            container = item;
-            var doughnutData = [{
-                value: 100,
-                color: "#757575"
-            }, {
-                value: 0,
-                color: "#bdbdbd"
-            }, ];
-        } else if (item == "sand") {
-            container = item;
-            var doughnutData = [{
-                value: 80,
-                color: "#757575"
-            }, {
-                value: 20,
-                color: "#bdbdbd"
-            }, ];
-        } else if (item == "water") {
-            container = item;
-            var doughnutData = [{
-                value: 20,
-                color: "#757575"
-            }, {
-                value: 80,
-                color: "#bdbdbd"
-            }, ];
-        }
-        var countries = document.getElementById(container).getContext("2d");
-        var doughnutOptions = {
-            segmentShowStroke: true,
-            animateScale: true,
-        };
-
-        new Chart(countries).Doughnut(doughnutData, doughnutOptions);
     });
+
 });
