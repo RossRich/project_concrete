c05927e1d8452a32b1b06d2bb2b876ee3d5d55a6
Hewlett Packard <Hewlett Packard>
2016-10-14 13:32:12 +0300

    вынесла блоки "наши кли", "доверие"; отступы в "наши достиж"

---
+++ includes/advantages_trust.php
@@ -0,0 +1,60 @@
+<div class="advantages_full">
+            <div class="uk-container2 uk-container-center advantages">
+                <h3 class="uk-text-center">Доверие, заслуженное качеством и технологиями</h3>
+                <ul class="uk-grid uk-grid-width-small-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-2" data-uk-grid-margin="{cls:'mt35'}">
+                    <li>
+                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
+                            <div class="ad_logo">
+                                <img src="images/ad1.png" alt="">
+                            </div>
+                            <div class="ad_text">
+                                <h4>СОВРЕМЕННОЕ ОБОРУДОВАНИЕ</h4>
+                                <p>Глубокий цикл отчистки компонентов бетона</p>
+                                <p>Точная система весового контроля загрузки миксеров</p>
+                                <p>Полное соответствие производства продукта по ГОСТу</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
+                            <div class="ad_logo">
+                                <img src="/images/ad2.png" alt="">
+                            </div>
+                            <div class="ad_text">
+                                <h4>СОБСТВЕННЫЙ ПАРК СПЕЦТЕХНИКИ</h4>
+                                <p>GPS контроль доставки в каждой машине</p>
+                                <p>Квалифицированные водители с допусками</p>
+                                <p>Собственный парк спецтехники: более 15 единиц</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
+                            <div class="ad_logo">
+                                <img src="/images/ad3.png" alt="">
+                            </div>
+                            <div class="ad_text">
+                                <h4>СОБСТВЕННАЯ ЛАБОРАТОРИЯ ЗАВОДА</h4>
+                                <p>Предоставление полной технической документации</p>
+                                <p>Проверка лабораторного образца на прочность</p>
+                                <p>100% юридическая ответственность за продукт</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
+                            <div class="ad_logo">
+                                <img src="/images/ad4.png" alt="">
+                            </div>
+                            <div class="ad_text">
+                                <h4>ГАРАНТИЯ НИЗКОЙ ЦЕНЫ</h4>
+                                <p>У нас всегда дешевле на 10%</p>
+                                <p>Быстрая доставка без простоя на объекте</p>
+                                <p>Гарантированное качество, за которое мы отвечаем</p>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
--- index.php
+++ index.php
@@ -21,66 +21,7 @@ $index_page_category = collection("Категории")->findOne(["items_index_p
 <body>
     <div class="wrapper uk-g">
         <? require($home_dir."/includes/header.php"); ?>
-        <div class="advantages_full">
-            <div class="uk-container2 uk-container-center advantages">
-                <h3 class="uk-text-center">Доверие, заслуженное качеством и технологиями</h3>
-                <ul class="uk-grid uk-grid-width-small-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-2" data-uk-grid-margin="{cls:'mt35'}">
-                    <li>
-                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
-                            <div class="ad_logo">
-                                <img src="images/ad1.png" alt="">
-                            </div>
-                            <div class="ad_text">
-                                <h4>СОВРЕМЕННОЕ ОБОРУДОВАНИЕ</h4>
-                                <p>Глубокий цикл отчистки компонентов бетона</p>
-                                <p>Точная система весового контроля загрузки миксеров</p>
-                                <p>Полное соответствие производства продукта по ГОСТу</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
-                            <div class="ad_logo">
-                                <img src="/images/ad2.png" alt="">
-                            </div>
-                            <div class="ad_text">
-                                <h4>СОБСТВЕННЫЙ ПАРК СПЕЦТЕХНИКИ</h4>
-                                <p>GPS контроль доставки в каждой машине</p>
-                                <p>Квалифицированные водители с допусками</p>
-                                <p>Собственный парк спецтехники: более 15 единиц</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
-                            <div class="ad_logo">
-                                <img src="/images/ad3.png" alt="">
-                            </div>
-                            <div class="ad_text">
-                                <h4>СОБСТВЕННАЯ ЛАБОРАТОРИЯ ЗАВОДА</h4>
-                                <p>Предоставление полной технической документации</p>
-                                <p>Проверка лабораторного образца на прочность</p>
-                                <p>100% юридическая ответственность за продукт</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="uk-panel uk-panel-box uk-clearfix ad_panel">
-                            <div class="ad_logo">
-                                <img src="/images/ad4.png" alt="">
-                            </div>
-                            <div class="ad_text">
-                                <h4>ГАРАНТИЯ НИЗКОЙ ЦЕНЫ</h4>
-                                <p>У нас всегда дешевле на 10%</p>
-                                <p>Быстрая доставка без простоя на объекте</p>
-                                <p>Гарантированное качество, за которое мы отвечаем</p>
-                            </div>
-                        </div>
-                    </li>
-
-                </ul>
-            </div>
-        </div>
+        <? include($home_dir."/includes/advantages_trust.php"); ?>
         <? 
         if(isset($index_page_category) && !empty($index_page_category)){ 
             $index_page_products = collection("Продукция")->find(["category" => $index_page_category["_id"]])->toArray();
@@ -137,39 +78,7 @@ $index_page_category = collection("Категории")->findOne(["items_index_p
             </div>
         </div>
         <? include($home_dir."/includes/consumer_about_us.php"); ?>
-        <div class="dev-contauner-our-partner dev-container-material uk-container-center">
-            <div class="uk-container uk-container-center dev-contauner-our-partner-correct dev-container-correct">
-                <h3 class="dev-h3-correct">НАШИ ПАРТНЕРЫ</h3>
-                <div class="uk-slidenav-position" data-uk-slider="center:true">
-                    <div class="uk-slider-container dev-our-partner-correct">
-                        <ul class="uk-slider uk-grid-width-large-1-5 uk-grid-width-medium-1-3 uk-grid-width-small-1-2">
-                            <li class="uk-active">
-                                <div class="dev-our-partner-panel-img-correct img_neom">
-                                </div>
-                            </li>
-                            <li>
-                                <div class="dev-our-partner-panel-img-correct imgASC">
-                                </div>
-                            </li>
-                            <li>
-                                <div class="dev-our-partner-panel-img-correct imgISM">
-                                </div>
-                            </li>
-                            <li>
-                                <div class="dev-our-partner-panel-img-correct imgFAMILY">
-                                </div>
-                            </li>
-                            <li>
-                                <div class="dev-our-partner-panel-img-correct imgCM">
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <img class="uk-slidenav uk-slidenav-contrast uk-slidenav-next dev-partner-slider-index-right-correct dev-partner-slider-index-height-correct dev-partner-ico-img-correct" data-uk-slider-item="next" src="/images/for_slider_nav_right.png">
-                    <img class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous dev-partner-slider-index-height-correct dev-partner-ico-img-correct" data-uk-slider-item="previous" src="/images/for_slider_nav_left.png">
-                </div>
-            </div>
-        </div>
+        <? include($home_dir."/includes/our_partners.php"); ?>
         <? include($home_dir."/includes/docs.php"); ?>
         <div class="prefooter"></div>
         <? include($home_dir."/includes/footer.php"); ?>
