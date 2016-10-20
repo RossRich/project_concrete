c2e05d61578cb4ab26c77dfe1c52650010f7b371
RossRich <leschenkobogdan@gmail.com>
2016-10-20 15:04:13 +0300

    pluged cookpit in our_partner

--- includes/our_partners.php
+++ includes/our_partners.php
@@ -1,34 +1,21 @@
+<?$our_partner=collection("Наши партнеры")->find(["visibility"=>true])->toArray();
+print_r($our_partner);
+?>
+
 <div class="dev-contauner-our-partner dev-container-material uk-container-center">
     <div class="uk-container-center dev-contauner-our-partner-correct dev-container-correct">
         <h3 class="dev-h3-correct">НАШИ ПАРТНЕРЫ</h3>
-        <div class="uk-slidenav-position" data-uk-slider="center:false">
+        <div class="uk-slidenav-position" data-uk-slider="center:false, infinite:false">
             <div class="uk-slider-container">
                 <ul class="uk-slider uk-grid uk-grid-width-large-1-5 uk-grid-width-medium-1-4 uk-grid-width-small-1-3" data-uk-grid-match="target:'.des-img-container'">
+                <? foreach ($our_partner as $key) {?>
+                    <? $img_path=$key["logotip"][0]["path"]; ?>
                     <li>
                         <div class="des-img-container">
-                            <img src="/images/ask_logo_text_150.png" width="152" height="74" alt="">
+                            <img src="<?=thumbnail_url($img_path, 154, 74 , ["mode"=>"best_fit"]);?>" width="152" height="74" alt="<?=$key["partner"] ?>">
                         </div>
                     </li>
-                    <li>
-                        <div class="des-img-container">
-                            <img src="/images/896ff0029059a66571a724317b4812d3.png" width="152" height="74" alt="">
-                        </div>
-                    </li>
-                    <li>
-                        <div class="des-img-container ttn">
-                            <img src="/images/D000008.png" width="152px" height="74px" alt="">
-                        </div>
-                    </li>
-                    <li>
-                        <div class="des-img-container">
-                            <img src="/images/ask_logo_text_150.png" width="152" height="74" alt="">
-                        </div>
-                    </li>
-                    <li>
-                        <div class="des-img-container">
-                            <img src="/images/ask_logo_text_150.png" width="152" height="74" alt="">
-                        </div>
-                    </li>
+                   <? } ?>
                 </ul>
             </div>
             <img class="uk-slidenav uk-slidenav-contrast uk-slidenav-next" data-uk-slider-item="next" src="/images/new_but_for_slider_right.png">
