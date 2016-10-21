b94c2b7d0aca49961fd013c077fdd811a2fe0e1e
Hewlett Packard <Hewlett Packard>
2016-10-21 17:40:18 +0300

    частично адаптивность карточек объекты

--- includes/facilities.php
+++ includes/facilities.php
@@ -7,13 +7,13 @@ $facilities = collection("Объекты")->find()->toArray();
 <div class="facilities_full_cs">
             <div class="uk-container2 uk-container-center facilities_cs">
                 <h3 class="dev-consumer-about-us-head-correct dev-h3-correct h3_cs">ОБЪЕКТЫ, ПОСТРОЕННЫЕ ИЗ НАШЕГО БЕТОНА</h3>
-                <div class="uk-slidenav-position facilities_slidenav_cs" data-uk-slider="center:true, infinite:true">
+                <div class="uk-slidenav-position facilities_slidenav_cs" data-uk-slider="infinite:true">
                     <div class="dev-consumer-slader-navigation uk-clearfix">
                         <img class="dev-consumer-icon " src="/images/ic_keyboard_arrow_right18dp.png" data-uk-slider-item="next">
                         <img class="dev-consumer-icon" src="/images/ic_keyboard_arrow_left_18dp.png" data-uk-slider-item="previous">
                     </div>
                     <div class="uk-slider-container">
-                        <ul class="uk-slider uk-grid uk-grid-width-large-1-3 uk-grid-width-medium-1-2 uk-grid-width-small-1-1 facilities_ul_slider_cs">
+                        <ul class="uk-slider uk-grid uk-grid-width-large-1-3 uk-grid-width-medium-1-2 uk-grid-width-small-1-2 facilities_ul_slider_cs">
 
                             <? foreach($facilities as $facility){ ?>
 
