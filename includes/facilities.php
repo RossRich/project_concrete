281fe38fa1e01e1e0ba92e4f3cabc6b4092e6899
Hewlett Packard <Hewlett Packard>
2016-10-21 14:48:57 +0300

    отказ от слайдсета, объекты в отдельный инклуд и вставила, шрифт тайтла в лайтбоксе

---
+++ includes/facilities.php
@@ -0,0 +1,135 @@
+<div class="facilities_full_cs">
+            <div class="uk-container2 uk-container-center facilities_cs">
+                <h3 class="dev-consumer-about-us-head-correct dev-h3-correct h3_cs">ОБЪЕКТЫ, ПОСТРОЕННЫЕ ИЗ НАШЕГО БЕТОНА</h3>
+                <div class="uk-slidenav-position facilities_slidenav_cs" data-uk-slider="center:true, infinite:true">
+                    <div class="dev-consumer-slader-navigation uk-clearfix">
+                        <img class="dev-consumer-icon " src="/images/ic_keyboard_arrow_right18dp.png" data-uk-slider-item="next">
+                        <img class="dev-consumer-icon" src="/images/ic_keyboard_arrow_left_18dp.png" data-uk-slider-item="previous">
+                    </div>
+                    <div class="uk-slider-container">
+                        <ul class="uk-slider uk-grid uk-grid-width-large-1-3 uk-grid-width-medium-1-2 uk-grid-width-small-1-1 facilities_ul_slider_cs">
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="facilities_1-li_cs">
+                                    <div class="uk-panel uk-panel-box dev-panel-correct facilities_panel_cs">
+                                        <a href="/images/f01.jpg" data-uk-lightbox="{group:'my-group'}" title="ЖК 'Дом'">
+                                        <figure class="uk-overlay uk-overlay-hover">
+                                        <img class="uk-overlay-scale" src="/images/objects.png" width="" height="" alt="">
+                                        <div class="uk-overlay-panel uk-overlay-icon facilities_overlay_icon"></div>
+                                        <figcaption class="uk-overlay-panel uk-overlay-bottom uk-ignore facilities_panel_bottom"><p>ЖК "Дом"</p>
+                                        <span>г. Краснодар</span>
+                                        </figcaption>
+                                        </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
